277575e0e40820bce8943bf1556cb627af069997
Gabriel A. Collado <gabriel.akacoll@gmail.com>
2020-12-24 13:29:03 -0300

    Green 3 - Desabilitar atualização da lista

--- main/FileList.java
+++ main/FileList.java
@@ -4,20 +4,27 @@ import java.util.ArrayList;
 public class FileList {
     private ArrayList<File> files;
     private int maxFiles;
+    private boolean isUpdateDisabled;
 
     public FileList(int maxFiles) {
         this.files = new ArrayList<File>();
         this.maxFiles = maxFiles;
+        this.isUpdateDisabled = false;
     }
 
     public int numfiles(){
         return this.files.size();
     }
 
+    public void disableUpdate(){
+        this.isUpdateDisabled = true;
+    }
+
     public void addFile(File file) {
         if(this.numfiles() >= this.maxFiles)
             this.files.remove(0);
-        this.files.add(file);
+        if(!this.isUpdateDisabled)
+            this.files.add(file);
     }
 
     public void removeFile(File file) {
