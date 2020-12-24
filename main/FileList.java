b00b8fe16bbb23e39e49d97ad4e143431cbc3692
Gabriel A. Collado <gabriel.akacoll@gmail.com>
2020-12-24 13:10:38 -0300

    Green 2 - Limitar número de arquivos abertos na lista

--- main/FileList.java
+++ main/FileList.java
@@ -8,17 +8,18 @@ public class FileList {
         this.files = new ArrayList<File>();
     }
 
+    public int numfiles(){
+        return this.files.size();
+    }
+
     public void addFile(File file) {
-//        if(!this.files.contains(file))
+        if(this.numfiles() >= 10)
+            this.files.remove(0);
         this.files.add(file);
     }
 
     public void removeFile(File file) {
         if(this.files.contains(file))
             this.files.remove(file);
-    }
-
-    public int numfiles(){
-        return this.files.size();
     }
 }
